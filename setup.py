2c05db445abd125f28c75a97955201a240519bab
Max <max@zapteryx.com>
2021-01-18 23:50:59 +0800

    alalal

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="something-to-do-with-quart", # Replace with your own username
+    version="1.0",
+    author="not me",
+    author_email="max@zapteryx.com",
+    description="i didn't make this",
+    long_description="what",
+    long_description_content_type="text/markdown",
+    url="https://github.com/zapteryx/quart-oauth2-discord",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
